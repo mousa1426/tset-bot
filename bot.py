e4ad87743952652d0d0a57050e369616c6345aae
mousa1426 <mousa.63@gmail.com>
2025-08-21 02:13:22 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,24 +1,14 @@
-import logging
+import os
 from telegram.ext import Updater, CommandHandler
 
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
-logger = logging.getLogger(__name__)
+TOKEN = os.getenv("BOT_TOKEN")
 
 def start(update, context):
-    update.message.reply_text("سلام! رباتت روی Render فعال شد 🚀")
+    update.message.reply_text("سلام! ربات روشنه 🚀")
 
-def main():
-    import os
-    TOKEN = os.getenv("BOT_TOKEN")
-    if not TOKEN:
-        raise ValueError("⚠️ BOT_TOKEN تنظیم نشده!")
-    updater = Updater(TOKEN, use_context=True)
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", start))
-    updater.start_polling()
-    updater.idle()
+updater = Updater(TOKEN, use_context=True)
+dp = updater.dispatcher
+dp.add_handler(CommandHandler("start", start))
 
-if __name__ == "__main__":
-    main()
+updater.start_polling()
+updater.idle()
